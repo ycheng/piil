99e347f45b17bc8d6e6b660950a6ee14dffa5263
Yuan-Chen Cheng <yc.cheng@canonical.com>
2017-05-04 17:10:46 +0800

    refactoring

--- piil.py
+++ piil.py
@@ -5,47 +5,52 @@
 import os.path
 import psutil
 
-# processes with informat we can read
-processes = dict()
-file_r_idx = dict()
-file_path_r_idx = dict()
+class ProcInfo:
+    """processes with informat we can read"""
+    processes = dict()
+    file_r_idx = dict()
+    file_path_r_idx = dict()
+    def add_ps_name(self, pid, ps_name):
+        """add process name"""
+        self.processes[pid] = ps_name
+    def addpath(self, path1, pid1):
+        """addpath: to index"""
+        base1 = os.path.basename(path1)
+        if base1 in self.file_r_idx:
+            self.file_r_idx[base1].add(path1)
+        else:
+            self.file_r_idx[base1] = {path1}
 
-def addpath(path1, pid1):
-    """addpath: to index"""
-    base1 = os.path.basename(path1)
-    if base1 in file_r_idx:
-        file_r_idx[base1].add(path1)
-    else:
-        file_r_idx[base1] = {path1}
+        if path1 in self.file_path_r_idx:
+            self.file_path_r_idx[path1].add(pid1)
+        else:
+            self.file_path_r_idx[path1] = {pid1}
 
-    if path1 in file_path_r_idx:
-        file_path_r_idx[path1].add(pid1)
-    else:
-        file_path_r_idx[path1] = {pid1}
+def main():
+    """main function"""
+    pinfo = ProcInfo()
+    for pid in psutil.pids():
+        ps1 = psutil.Process(pid)
+        pinfo.add_ps_name(pid, ps1.name)
+        try:
+            ofs = ps1.open_files()
+            ofmaps = ps1.memory_maps()
 
-for pid in psutil.pids():
-    p = psutil.Process(pid)
-    processes[pid] = p.name()
-    try:
-        ofs = p.open_files()
-        ofmaps = p.memory_maps()
-
-        for of in ofs:
-            addpath(of.path, pid)
-        for ofm in ofmaps:
-            addpath(ofm.path, pid)
-    except (psutil.AccessDenied, psutil.NoSuchProcess, PermissionError):
-        # print(process.name() + " Can't be read")
-        pass
-    else:
-        pass
-
-#print(processes)
-print("======================")
-
-
-print(file_r_idx)
+            for of1 in ofs:
+                pinfo.addpath(of1.path, pid)
+            for ofm in ofmaps:
+                pinfo.addpath(ofm.path, pid)
+        except (psutil.AccessDenied, psutil.NoSuchProcess, PermissionError):
+            # print(process.name() + " Can't be read")
+            pass
+        else:
+            pass
+    print("======================")
+    print(pinfo.file_r_idx)
 
 # p.open_files()
 # p.memory_maps
 # psutil.AccessDenied
+
+if __name__ == '__main__':
+    main()
